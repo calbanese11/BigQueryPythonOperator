9c90cf1d2acb2fa546250f479ae55395b4a6b16a
Cody Albanese <calbanese11@gmail.com>
2021-10-05 07:27:11 -0500

    Couple of edits

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+setuptools.setup(setup_requires=['pbr'], pbr=True)
+
+
+# classifiers=[
+#         'Programming Language :: Python',
+#         'Programming Language :: Python :: 3',
+#         'Intended Audience :: Developers',
+#         'License :: OSI Approved :: Apache Software License',
+#         'Operating System :: OS Independent',
+#         'Topic :: Software Development :: Libraries :: Python Modules',
+#     ],
